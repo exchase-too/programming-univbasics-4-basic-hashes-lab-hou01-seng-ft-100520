8bbdf612000235608790d38de1402bc6a4a1fae3
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-15 19:41:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,11 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer_hash = {
+    :name => "Grace Hopper"
+  }
+  pioneer_hash
 end
-
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
